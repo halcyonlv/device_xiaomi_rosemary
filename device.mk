d1c1305c89fd972e8a9027b8467b8cec59c9f2a0
Mashopy <eliasgheeraert@gmail.com>
2026-04-08 18:16:32 +0200

    rosemary: Inherit telephony-common-stub
    Google deprecated TelephonyMetrics class in Android 16 QPR2 which our IMS stack depends on.
    
    Change-Id: I78d8e053bfc3b3f492919a89a6b5e1114614c4e8

--- device.mk
+++ device.mk
@@ -151,6 +151,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # IMS
+$(call inherit-product, hardware/lineage/compat/frameworks/compat.mk)
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/mediatek-common.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/mediatek-common.xml \
     $(LOCAL_PATH)/configs/permissions/mediatek-framework.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/mediatek-framework.xml \
