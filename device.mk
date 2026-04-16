8df88de9d41480e0763994c4218cb95f65139368
Nolen Johnson <johnsonnolen@gmail.com>
2026-04-16 09:46:24 +0200

    rosemary: Build mediatek-common JAR from source
    Reference: I7967d31f0d1799610c6d31d4d6e48f0ccc77a7f2
    Change-Id: I671cb1758288f6ca565495366c6a835a6e7a0ec5

--- device.mk
+++ device.mk
@@ -152,6 +152,7 @@ PRODUCT_COPY_FILES += \
 
 # IMS
 $(call inherit-product, hardware/lineage/compat/frameworks/compat.mk)
+$(call inherit-product, hardware/mediatek/frameworks/mediatek-frameworks.mk)
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
