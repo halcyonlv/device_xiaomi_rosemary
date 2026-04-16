8cff99a964b272dacd0f1d14db9d1e43f4875a5a
Giovanni Ricca <giovanniricca@proton.me>
2026-04-16 09:40:25 +0200

    rosemary: Move mediatek ims boot jar declaration to vendor
    Change-Id: I8eb961ac31326eaadd2bb3d10d2ecbe2e95c233b

--- device.mk
+++ device.mk
@@ -153,15 +153,6 @@ PRODUCT_COPY_FILES += \
 # IMS
 $(call inherit-product, hardware/lineage/compat/frameworks/compat.mk)
 
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
 
